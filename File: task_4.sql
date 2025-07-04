5df784e566f47e6619f63fdc7ad18e532fe86854
Ernest Nyarko <127901738+Henrynyarko@users.noreply.github.com>
2025-07-05 00:28:26 +0100

    Update File: task_4.sql

--- File: task_4.sql
+++ File: task_4.sql
@@ -1,28 +1,4 @@
-import mysql.connector
-from mysql.connector import Error
-
-try:
-    # Connect to MySQL Server
-    connection = mysql.connector.connect(
-        host='localhost',
-        user='your_mysql_username',
-        password='your_mysql_password'
-    )
-
-    if connection.is_connected():
-        cursor = connection.cursor()
-        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-        print("Database 'alx_book_store' created successfully!")
-
-except mysql.connector.Error as err:
-    print(f"Error while connecting to MySQL: {err}")
-
-finally:
-    if 'connection' in locals() and connection.is_connected():
-        cursor.close()
-        connection.close()
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'books'
+WHERE TABLE_NAME = 'Books'
   AND TABLE_SCHEMA = DATABASE();
-
